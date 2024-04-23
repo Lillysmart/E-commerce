0a01168a840e1a23295f84ea0085880ca7e2ea55
Lillysmart <137174363+Lillysmart@users.noreply.github.com>
2024-04-23 16:20:01 +0200

    Import the css navbar

--- Front-End/src/Components/NavBar/NavBar/navbar.jsx
+++ Front-End/src/Components/NavBar/NavBar/navbar.jsx
@@ -1,6 +1,7 @@
-import React from 'react';
-import logo from '../../../../Assets/logo.png' ;
-//import CartIcon from '../../../../Assets/cart-icon.png';
+import React from "react";
+import logo from "../../../../Assets/logo.png";
+import CartIcon from '../../../../Assets/cart_icon.png';
+import "./navbar.css"
 
 const Navbar = () => {
   return (
@@ -9,13 +10,17 @@ const Navbar = () => {
         <img src={logo} alt="Logo" /> {/* Use the imported image variable */}
         <p>Shopper</p>
       </div>
-     <ul className='nav-menu' >
+      <ul className="nav-menu">
         <li>Shop</li>
         <li>Men</li>
         <li>Women</li>
         <li>Kids</li>
+      </ul>
+      <div className="nav-login-cart">
+        <button>Login</button>
+        <img src={CartIcon}/>
 
-     </ul>
+      </div>
     </div>
   );
 };
