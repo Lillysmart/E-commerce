c3667445b7c715eb261ca907880e48bbb6b47ea1
Lillysmart <137174363+Lillysmart@users.noreply.github.com>
2024-04-23 16:10:54 +0200

    Correctly import the images

--- Front-End/src/Components/NavBar/NavBar/navbar.jsx
+++ Front-End/src/Components/NavBar/NavBar/navbar.jsx
@@ -1,14 +1,16 @@
-
 import React from 'react';
-import "./navbar.css"
+import logo from '../../../../Assets/logo.png' ;
+//import CartIcon from '../../../../Assets/cart-icon.png';
 
-const NavBar = () => {
+const Navbar = () => {
   return (
-    <div>
-    <p>Hi there</p>
+    <div className="nav-bar">
+      <div className="nav-logo">
+        <img src={logo} alt="Logo" /> {/* Use the imported image variable */}
+        <p>Shopper</p>
+      </div>
     </div>
   );
 };
 
-export default NavBar ;
-
+export default Navbar;
