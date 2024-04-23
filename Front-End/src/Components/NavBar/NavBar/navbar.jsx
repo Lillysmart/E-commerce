a25086153a66a5be648847b44ec8f036de35f006
Lillysmart <137174363+Lillysmart@users.noreply.github.com>
2024-04-23 15:23:05 +0200

    import the nabvar component

--- Front-End/src/Components/NavBar/NavBar/navbar.jsx
+++ Front-End/src/Components/NavBar/NavBar/navbar.jsx
@@ -1,10 +1,11 @@
 
 import React from 'react';
+import "./navbar.css"
 
 const NavBar = () => {
   return (
     <div>
-      {/* Your component code here */}
+    <p>Hi there</p>
     </div>
   );
 };
