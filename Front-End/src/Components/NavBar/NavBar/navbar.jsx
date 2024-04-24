9b82a1f43f97ffc604feead780701419be9e6835
Lillysmart <137174363+Lillysmart@users.noreply.github.com>
2024-04-24 15:26:56 +0200

    Add the event listener for the <hr/>

--- Front-End/src/Components/NavBar/NavBar/navbar.jsx
+++ Front-End/src/Components/NavBar/NavBar/navbar.jsx
@@ -1,26 +1,32 @@
-import React from "react";
+import React, { useState } from "react";
 import logo from "../../../../Assets/logo.png";
 import CartIcon from '../../../../Assets/cart_icon.png';
 import "./navbar.css"
 
 const Navbar = () => {
+  const [menu , setMenu]= useState("shop")
+  
   return (
+    
     <div className="nav-bar">
       <div className="nav-logo">
         <img src={logo} alt="Logo" /> {/* Use the imported image variable */}
         <p>Shopper</p>
       </div>
       <ul className="nav-menu">
-        <li>Shop <hr/></li>
-        <li>Men</li>
-        <li>Women</li>
-        <li>Kids</li>
+        <li onClick={()=>{setMenu("Shop")}}> Shop{menu ==="Shop"? <hr/> :<></> } </li>
+        <li onClick={()=>{setMenu("Mens")}} >Men</li>
+        <li onClick={()=>{setMenu("Women")}}>Women</li>
+        <li onClick={()=>{setMenu("Kids")}}>Kids</li>
       </ul>
       <div className="nav-login-cart">
         <button>Login</button>
         <img src={CartIcon}/>
+        <div className="nav-cart-count">0
 
       </div>
+      </div>
+      
     </div>
   );
 };
