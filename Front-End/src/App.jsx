ae56a636f395cd8262484c777cbeb831d42bcfc1
Lillysmart <137174363+Lillysmart@users.noreply.github.com>
2024-05-02 13:16:39 +0200

    Add the routes

--- Front-End/src/App.jsx
+++ Front-End/src/App.jsx
@@ -3,12 +3,28 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import NavBar from './Components/NavBar/NavBar/navbar' 
+import {BrowserRouter , Route , Routes } from 'react-router-dom'
+import Shop from './Pages/Shop'
+import Product from './Pages/Product'
+import ShopCategory from './Pages/ShopCategory'
+import Cart from './Pages/Cart'
+import LoginSignUp from './Pages/LoginSignUp'
+
 
 const App=()=> {
   
   return (
     <>
-      <NavBar/>
+    <BrowserRouter>
+    <NavBar/>
+    <Routes>
+      <Route path='/' element={<Shop/>} />
+      <Route path='/mens'  element={<ShopCategory/>} />
+      <Route path='/womens'  element={<ShopCategory/>} />
+      <Route path='/kids'  element={<ShopCategory/>} />
+    </Routes>
+    </BrowserRouter>
+      
        
     </>
   )
